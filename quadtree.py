03f92863be0da495f33d3f08cb0b5ba7c03d91a6
Timothy Chan Yan Mong <timothychan29475600@gmail.com>
2022-07-25 11:15:14 +0800

    Implemented rect intersection in quadtree
    Implemented code to find the intersection between points and any rectangular region.

--- quadtree.py
+++ quadtree.py
@@ -13,6 +13,23 @@ def in_rect(point, rect):
     """
     return (0 <= point.x - rect.x) and (point.x - rect.x < rect.w) and (0 <= point.y - rect.y) and (point.y-rect.y < rect.h)
 
+def rect_intersect(rect1,rect2):
+    if rect2.x >= rect1.x:
+        rect_x1 = rect1
+        rect_x2 = rect2
+    else:
+        rect_x1 = rect2
+        rect_x2 = rect1
+    
+    if rect2.y >= rect1.y:
+        rect_y1 = rect1
+        rect_y2 = rect2
+    else:
+        rect_y1 = rect2
+        rect_y2 = rect1
+    return (rect_x2.x - rect_x1.x <= rect_x1.w) and (rect_y2.y-rect_y1.y <= rect_y1.h)
+
+
 
 class Quadtree:
     def __init__(self,box,cap=4,depth=0):
@@ -22,6 +39,12 @@ class Quadtree:
         self.point_count = 0
         self.nodes = {}
         self.depth = depth
+    
+    def __del__(self):
+        # Remove everything except the points
+        del self.box
+        for _, node in self.nodes.items():
+            del node
     
     def draw(self,screen):
         pygame.draw.rect(screen,(255,255,255),
@@ -35,26 +58,26 @@ class Quadtree:
         self.points.append(point)
         self.point_count += 1 # Count all points under the box
 
-        print(f'{lv_sep}Points list: ',self.points)
+        #print(f'{lv_sep}Points list: ',self.points)
         if self.point_count > self.cap:
             # Create a new layer of recursion and transfer the points to child
-            print(f'{lv_sep}Recursion!!')
+            #print(f'{lv_sep}Recursion!!')
             # If no child, create child
             if len(self.nodes.keys()) == 0:
                 x,y,w,h = self.box.x, self.box.y, self.box.w, self.box.h
-                self.nodes['NW'] = Quadtree(Rectangle(x,y,w/2,h/2),depth=self.depth+1)
-                self.nodes['NE'] = Quadtree(Rectangle(x+w/2,y,w/2,h/2),depth=self.depth+1)
-                self.nodes['SW'] = Quadtree(Rectangle(x,y+h/2,w/2,h/2),depth=self.depth+1)
-                self.nodes['SE'] = Quadtree(Rectangle(x+w/2,y+h/2,w/2,h/2),depth=self.depth+1)
-                print(f'{lv_sep}Created new subtrees')
+                self.nodes['NW'] = Quadtree(Rectangle(x,y,w/2,h/2),cap=self.cap,depth=self.depth+1)
+                self.nodes['NE'] = Quadtree(Rectangle(x+w/2,y,w/2,h/2),cap=self.cap,depth=self.depth+1)
+                self.nodes['SW'] = Quadtree(Rectangle(x,y+h/2,w/2,h/2),cap=self.cap,depth=self.depth+1)
+                self.nodes['SE'] = Quadtree(Rectangle(x+w/2,y+h/2,w/2,h/2),cap=self.cap,depth=self.depth+1)
+                #print(f'{lv_sep}Created new subtrees')
 
             # Recursive insert
             inserted = False
-            print(f'{lv_sep}Recursive insert: ')
+            #print(f'{lv_sep}Recursive insert: ')
             for pt in self.points:
-                print(f'{lv_sep}Point: ',pt)
+                #print(f'{lv_sep}Point: ',pt)
                 for key, node in self.nodes.items():
-                    print(f'{lv_sep}\tkey: ',key,'in box ',in_rect(pt,node.box))
+                    #print(f'{lv_sep}\tkey: ',key,'in box ',in_rect(pt,node.box))
                     if in_rect(pt,node.box):
                         node.insert(pt)
                         inserted = True
@@ -63,5 +86,19 @@ class Quadtree:
             
             self.points = []
 
-        
+    def get_particle_in_rect(self,rect):
+        queue = [self]
+        result = []
+        while len(queue) != 0:
+            entry = queue.pop(0)
+            if rect_intersect(entry.box,rect):
+                if len(entry.nodes.keys()) == 0:
+                   for pt in entry.points:
+                     if in_rect(pt,rect):
+                        result.append(pt)
+                else:
+                    for _,node in entry.nodes.items():
+                        queue.append(node)
+        return result
+
     
